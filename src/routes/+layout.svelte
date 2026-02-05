f85ce64bdaadf0a063a820c0a7c09575d0c97c96
Enzo Boursier <enzoboursier@outlook.fr>
2026-02-05 16:52:17 +0100

    test background 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,31 @@
 	let { children } = $props();
 </script>
 
-<Nav />
-{@render children()}
+<div class="app">
+	<Nav />
+	{@render children()}
+</div>
+
+<style lang="scss">
+	:global(html) {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		overflow-x: hidden;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		overflow-x: hidden;
+	}
+
+	.app {
+		width: 100%;
+		min-height: 100vh;
+		margin: 0;
+		padding: 0;
+		overflow-x: hidden;
+	}
+</style>
